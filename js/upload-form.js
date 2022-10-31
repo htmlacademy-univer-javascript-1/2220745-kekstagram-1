263e9076eaf3611377c65faaa8a93b6e6dad9227
Kirionenko <misha.kirionenko03@gmail.com>
2022-10-31 18:57:33 +0500

    Правда или действие (часть 1) (ред)

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,23 +1,10 @@
-const uploadForm = document.querySelector('.img-upload__form');
-uploadForm.method = 'post';
-uploadForm.action = 'https://26.javascript.pages.academy/kekstagram';
-
 const uploadControl = document.querySelector('.img-upload__start');
 const editForm = document.querySelector('.img-upload__overlay');
 const editFormCloseButton = editForm.querySelector('.img-upload__cancel');
-const inputHashtags = editForm.querySelector('.text__hashtags');
-const inputDescription = editForm.querySelector('.text__description');
 
-let isInputBoxesOnFocus = false;
-inputHashtags.onfocus = () => {isInputBoxesOnFocus = true;};
-inputHashtags.onblur = () => {isInputBoxesOnFocus = false;};
-inputDescription.onfocus = () => {isInputBoxesOnFocus = true;};
-inputDescription.onblur = () => {isInputBoxesOnFocus = false;};
+editForm.querySelector('.img-upload__text').addEventListener('keydown', (evt) => evt.stopPropagation());
 
 const onDocumentEscKeyDown  = (evt) => {
-  if (isInputBoxesOnFocus) {
-    return;
-  }
   if (evt.key === 'Escape') {
     editForm.classList.add('hidden');
     document.body.classList.remove('modal-open');
